fd2e72a260b205b99dd3b0939d003158732a981d
aryan-212 <allalonearyan@gmail.com>
2025-08-02 10:14:31 +0530

    feat(client) : added logout functionality

--- src/connection_initialisation.rs
+++ src/connection_initialisation.rs
@@ -1,4 +1,6 @@
+use reqwest::Error;
 use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct LoginClient {
     pub client: reqwest::Client,
@@ -16,22 +18,54 @@ impl LoginClient {
         }
     }
 
-    pub async fn login(&self, username: &str, password: &str, url: &str) -> Result<String, String> {
+    pub async fn login(&self, username: &str, password: &str, url: &str) -> Result<String, Error> {
+        let millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            .to_string();
+            
         let mut form_data = HashMap::new();
         form_data.insert("mode", "191");
         form_data.insert("username", username);
         form_data.insert("password", password);
-        form_data.insert("a", "1713188925839");
+        form_data.insert("a", &millis);
         form_data.insert("producttype", "0");
 
-        self.client
+        let res = self.client
             .post(url)
             .form(&form_data)
             .send()
-            .await
-            .map_err(|e| e.to_string())?
+            .await?
             .text()
-            .await
-            .map_err(|e| e.to_string())
+            .await?;
+        println!("Login Response: {}", res);
+        Ok(res)
+    }
+    
+    pub async fn logout(&self, username: &str, password: &str, url: &str) -> Result<String, Error> {
+        let millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            .to_string();
+            
+        let mut form_data = HashMap::new();
+        form_data.insert("mode", "193"); // Different mode for logout
+        form_data.insert("username", username);
+        form_data.insert("password", password);
+        form_data.insert("a", &millis);
+        form_data.insert("producttype", "0");
+        
+        let res = self
+            .client
+            .post(url)
+            .form(&form_data)
+            .send()
+            .await?
+            .text()
+            .await?;
+        println!("Logout Response: {}", res);
+        Ok(res)
     }
 }
